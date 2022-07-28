ebf9661262b7e483242b87867907a6d607b245f7
Inaki Villar <inaki.seri@gmail.com>
2022-07-27 17:17:09 -0700

    adding README

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -2,6 +2,19 @@ plugins {
     id("com.gradle.example.rewritekey")
 }
 
+tasks.register<com.gradle.example.codegenerator.JavaGeneratorTask>("javaGeneratorTask") {
+    packageName.set("com.example.gradle")
+    classes.set(
+        mapOf(
+            "Example" to "class Example { public String getApiKey() {return \"ANOTHER_DEPLOYMENT_KEY\"; } }",
+            "ExampleNoApiKey" to "class ExampleNoApiKey { }",
+        )
+    )
+    outputDirectory.set(layout.projectDirectory.dir("generated/java"))
+}
+
+java.sourceSets["main"].java.srcDir(tasks.named("javaGeneratorTask"))
+
 dependencies {
-    testImplementation("junit:junit:4.13")
+    testImplementation("junit:junit:4.13.2")
 }
